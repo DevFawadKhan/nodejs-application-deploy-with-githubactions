a90b9caff0ec9d7635e5f0db36811a6e9e084664
@Fawad228 <mfawadkhan05@gmail.com>
2026-02-21 01:29:40 +0500

    new endpoint

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT ?? 8080;
 app.get("/", (req, res) => {
   return res.json({ msg: "Hello from the server v2 deployed to EC2!\n" });
 });
+app.get("/ec2", (req, res) => {
+  return res.json({ msg: "This is the EC2 endpoint!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on PORT ${PORT}`);
